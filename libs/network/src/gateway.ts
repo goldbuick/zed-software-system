a5860289fbe58cc6d0d0237de538accb8e0da4cf
goldbuick <whitlark@gmail.com>
2023-04-03 10:19:25 -0400

    Simplifying things further

--- libs/network/src/gateway.ts
+++ libs/network/src/gateway.ts
@@ -1,87 +1,54 @@
 /*
 
-This file manages the mechnaism that allows nodes to bootstrap into the network
+Do we even need k-buckets ??
+Is XOR distance a sufficient metric ?
 
-1.We connect to the bootstrap node and issue a JOIN message with our id
-  This message is routed towards our id, we are essentially trying to send a message to ourself
-  Nodes along the way, should connect temporarily to the joining node so the joining node can have their
-  ID added to their routing table
-2.We stay connected to (X) number of closest peers
-3.This is simply a messaging network designed at handling larger scale of users
-
-Idea for how client/server routing would work
-1.client sends a SUB message to given ID
-2.intermediate nodes build a SUB lookup table
-  this is a way to send the message back to nodes interested in messages
+v1 approach
+  We poll for an updated list of peers
+  We connect to the 3 closest to our ID
+  We route messages accordingly
+    We still need PUB/SUB tables
 
 */
 
-import Peer from 'peerjs'
-import { nanoid, customAlphabet } from 'nanoid'
-import { nolookalikes } from 'nanoid-dictionary'
-import { KBuckets } from './k-buckets'
+import Peer, { PeerJSOption } from 'peerjs'
 
-const createPeerId = customAlphabet(nolookalikes, 32)
+const PEER_JS_OPTIONS: PeerJSOption = {
+  debug: 1,
+  port: 443,
+  secure: true,
+  key: 'peerjs',
+  host: 'nest.ili.ac',
+}
 
-const BOOSTRAP_ID = '5loXSIFThHidEIEh6h89NQEZpdqI4X4B'
-
-const PEER_JS_OPTIONS = {
-  // host: 'zed-cafe-connect.herokuapp.com',
-  // secure: true,
-  // port: 443,
-  debug: 0,
-  // config: {
-  //   iceServers: [
-  //     { urls: 'stun:stun.l.google.com:19302' },
-  //     // {
-  //     //   urls: [
-  //     //     'turn:eu-0.turn.peerjs.com:3478',
-  //     //     'turn:us-0.turn.peerjs.com:3478',
-  //     //   ],
-  //     //   username: 'peerjs',
-  //     //   credential: 'peerjsp',
-  //     // },
-  //   ],
-  //   sdpSemantics: 'unified-plan',
-  // },
+function randomBits() {
+  return (Math.random() * 4294967296) >>> 0
 }
 
 export class Gateway {
   private peer: Peer
 
   static createPeerId() {
-    return createPeerId()
+    return `zss-${randomBits().toString(16)}`
   }
 
   constructor() {
-    this.peer = this.host()
     window.addEventListener('beforeunload', this.destroy)
-  }
 
-  destroy = () => {
-    this.peer.destroy()
-    window.removeEventListener('beforeunload', this.destroy)
-  }
-
-  host = () => {
-    // reset
-    if (this.peer) {
-      this.peer.destroy()
-    }
-
-    // default is to create as host
-    this.peer = new Peer(BOOSTRAP_ID, PEER_JS_OPTIONS)
+    this.peer = new Peer(Gateway.createPeerId(), PEER_JS_OPTIONS)
 
     this.peer.on('open', () => {
-      console.info('host: ready on', this.peer.id)
-      this.peer.on('close', () =>
-        this.retry('connection to handshake server lost'),
-      )
+      this.peer.on('close', () => {
+        // signal something ??
+      })
+
+      console.info('gateway: ready on', this.peer.id)
+      this.join()
     })
 
     this.peer.on('connection', (dataConnection) => {
       dataConnection.on('open', () => {
-        console.info('host: connection from', dataConnection.peer)
+        console.info('gateway: connection from', dataConnection.peer)
 
         dataConnection.on('close', () => {
           // clean up
@@ -94,59 +61,26 @@ export class Gateway {
     })
 
     this.peer.on('error', (error) => {
-      console.error('host: error', error.type, error)
-      // if (error.type === 'unavailable-id') {
-      // fallback to join
-      // }
-      this.join()
+      console.error('gateway: error', error.type, error)
     })
-
-    return this.peer
   }
 
-  join() {
-    // reset
-    if (this.peer) {
-      this.peer.destroy()
+  destroy = () => {
+    this.peer.destroy()
+    window.removeEventListener('beforeunload', this.destroy)
+  }
+
+  async join() {
+    try {
+      const apiUrl = `${PEER_JS_OPTIONS.secure ? 'https' : 'http'}://${
+        PEER_JS_OPTIONS.host
+      }:${PEER_JS_OPTIONS.port}/${PEER_JS_OPTIONS.key}/peers`
+      const response = await fetch(apiUrl)
+      const result = await response.json()
+      //
+      console.info(result)
+    } catch (error) {
+      console.info('gateway: error fetching peer list', error)
     }
-
-    // try to join gateway node
-    this.peer = new Peer('', PEER_JS_OPTIONS)
-
-    this.peer.on('open', () => {
-      console.info('join: ready on', this.peer.id)
-      console.info('join: trying', BOOSTRAP_ID)
-      const dataConnection = this.peer.connect(BOOSTRAP_ID)
-
-      dataConnection.on('open', () => {
-        console.info('join: connection from', dataConnection.peer)
-
-        dataConnection.on('close', () => {
-          // signal retry
-        })
-
-        dataConnection.on('data', ({ msg, data }) => {
-          // handle messages here
-        })
-      })
-
-      this.peer.on('close', () =>
-        this.retry('connection to handshake server lost'),
-      )
-    })
-
-    this.peer.on('error', (error) => {
-      console.error('join: error', error.type, error)
-      if (error.type === 'peer-unavailable') {
-        this.retry('unable to connect to host')
-      }
-    })
-
-    return this.peer
-  }
-
-  retry = (reason: string) => {
-    console.info('retrying', reason)
-    setTimeout(this.host, 5000)
   }
 }
