ee3d1b3e2064615856f18d5f82bc9aae309af999
goldbuick <whitlark@gmail.com>
2023-07-10 21:38:49 -0400

    working on runtime data model

--- libs/terminal/src/data/runtime.ts
+++ libs/terminal/src/data/runtime.ts
@@ -1,9 +1,11 @@
-/*
+import { PACKAGE } from './format'
 
-used to actively run software ...
-
-*/
-
-export function noop() {
-  //
+// we load packages into the workspace
+export type WORKSPACE = {
+  // primary package being run
+  // deep-cave@1
+  main: string
+  // this is index by name + version
+  // daves-rpg@1 -> PACKAGE
+  packages: Record<string, PACKAGE>
 }
