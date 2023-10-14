92874567a222aaaa20771faf950f05ae0f405d51
goldbuick <whitlark@gmail.com>
2023-10-14 03:22:00 -0400

    fiddling

--- zss/terminal/app.tsx
+++ zss/terminal/app.tsx
@@ -70,7 +70,7 @@ const eventManagerFactory: Parameters<typeof Canvas>[0]['events'] = (
   },
 })
 
-const PHASE = 0.0006
+const PHASE = 0.0008
 
 export function App() {
   const [ref, bounds] = useMeasure()
