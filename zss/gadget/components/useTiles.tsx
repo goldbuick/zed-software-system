a90a3d7c33f461defeb99e83bbcdc54ad6572017
goldbuick <whitlark@gmail.com>
2023-10-27 18:02:20 -0400

    useTiles updated

--- zss/gadget/components/useTiles.tsx
+++ zss/gadget/components/useTiles.tsx
@@ -1,6 +1,18 @@
-/*
+import { useMemo } from 'react'
+import { proxy } from 'valtio'
 
-What is useTiles, it is a simple hook where you give it width & height
-and in turn will give you a proxy TILES object, that will re-render when changed
+import { TILES } from '../data/types'
 
-*/
+export function useTiles(width: number, height: number): TILES {
+  const size = width * height
+
+  const tiles = useMemo(() => {
+    return proxy<TILES>({
+      char: new Array(size).fill(0),
+      color: new Array(size).fill(0),
+      bg: new Array(size).fill(0),
+    })
+  }, [width, height])
+
+  return tiles
+}
