d1974f0730d9a7ece28717efe55bc0073ec13c7f
goldbuick <whitlark@gmail.com>
2023-11-02 10:50:47 -0400

    wobble fx

--- zss/terminal/components/FX.tsx
+++ zss/terminal/components/FX.tsx
@@ -1,17 +1,47 @@
+import { useFrame } from '@react-three/fiber'
 import {
   EffectComposer,
   ChromaticAberration,
 } from '@react-three/postprocessing'
-import { BlendFunction } from 'postprocessing'
+import { ChromaticAberrationEffect } from 'postprocessing'
 import React from 'react'
 
-const PHASE = 0.0007 / window.devicePixelRatio
+let PHASE = 0.005
 
 export function FX() {
+  // useEffect(() => {
+  //   const timer = setInterval(() => {
+  //     PHASE = Math.max(0.1, Math.min(0.01, PHASE + Math.random() * 0.001))
+  //   }, 100)
+  //   return () => {
+  //     clearInterval(timer)
+  //   }
+  // }, [])
+
+  const ref = React.createRef<typeof ChromaticAberrationEffect>()
+
+  useFrame((state, delta) => {
+    if (!ref.current) {
+      return
+    }
+
+    PHASE = PHASE + (1 - Math.random() * 2) * delta * 0.01
+    PHASE = Math.max(0.0001, Math.min(0.007, PHASE))
+    console.info(PHASE)
+
+    ref.current.offset = [
+      -PHASE / window.devicePixelRatio,
+      PHASE / window.devicePixelRatio,
+    ]
+    ref.current.modulationOffset = PHASE
+  })
+
   return (
     <EffectComposer>
       <ChromaticAberration
-        blendFunction={BlendFunction.NORMAL} // blend mode
+        ref={ref}
+        radialModulation
+        modulationOffset={0.5}
         // @ts-expect-error numbers !
         offset={[-PHASE, PHASE]} // color offset
       />
