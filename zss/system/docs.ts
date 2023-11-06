0b99c6fc6e269fcaea17be77c8eaf038043101b4
goldbuick <whitlark@gmail.com>
2023-11-06 18:16:22 -0500

    ugh

---
+++ zss/system/docs.ts
@@ -0,0 +1,6 @@
+/*
+
+What are system docs?
+yjs docs that are used to collab edit code page content
+
+*/
---
+++ zss/system/vm.ts
@@ -0,0 +1,6 @@
+/*
+
+What is a VM? a vm runs / executes a collection of code pages
+and includes needed run-time state
+
+*/
