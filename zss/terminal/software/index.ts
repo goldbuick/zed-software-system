73e2601bcedb8e267540947a8dc2a39e040c824b
goldbuick <whitlark@gmail.com>
2023-11-16 19:02:29 -0500

    notes

--- zss/terminal/software/index.ts
+++ zss/terminal/software/index.ts
@@ -5,6 +5,20 @@ import { range } from 'zss/mapping/array'
 import { createGuid } from 'zss/mapping/guid'
 import { CODE_PAGE, CODE_PAGE_TYPE } from 'zss/system/codepage'
 
+/*
+
+We have 3 multi-user interaction categories here
+
+1.  LOGIN, terminal user, sends input to hubworker, worker sends gadget state in return
+    terminals support multiple users logging in
+
+2.  PILOT, sim user, sends input to SESSION, SESSION sends relevant sim state
+    a sim supports multiple users piloting
+
+3.  CREATE, edit user, sends changes to SESSION, SESSION sends relevant edit state
+     
+*/
+
 export const TAPE_PAGES: CODE_PAGE[] = [
   {
     id: createGuid(),
