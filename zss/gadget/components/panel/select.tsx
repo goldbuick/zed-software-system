8ed3bfc811ab468682d1864bdfedf0002e85feb9
goldbuick <whitlark@gmail.com>
2023-12-11 21:35:15 -0500

    mocked text input

--- zss/gadget/components/panel/select.tsx
+++ zss/gadget/components/panel/select.tsx
@@ -1,11 +1,66 @@
-import { tokenizeAndWriteTextFormat } from '../../data/textFormat'
+import React, { useCallback, useState } from 'react'
 
-import { PanelItemProps } from './common'
+import {
+  cacheWriteTextContext,
+  tokenizeAndWriteTextFormat,
+  writeCharToEnd,
+} from '../../data/textFormat'
+import { UserInput, UserInputHandler } from '../userinput'
 
-export function PanelItemSelect({ label, args, context }: PanelItemProps) {
+import { PanelItemProps, mapTo, strsplice, useBlink } from './common'
+
+export function PanelItemSelect({
+  active,
+  label,
+  args,
+  context,
+}: PanelItemProps) {
+  const [target, ...values] = [mapTo(args[0], ''), ...args.slice(1)]
+
+  const min = 0
+  const max = values.length - 1
+
+  // where does the current value live here ??
+  const blink = useBlink()
+  const [value, setValue] = useState(0)
+  const tvalue = `${values[value]}`
+  const tlabel = label.trim()
+  const tcolor = active ? 'grey' : 'white'
+
+  // keep stable re-renders
+  cacheWriteTextContext(context)
+
   tokenizeAndWriteTextFormat(
-    `$white${label} $green${(args[0] ?? '').toUpperCase()}`,
+    `$green${tvalue.padStart(3).padEnd(4)}$${tcolor}${tlabel} \\`,
     context,
   )
-  return null
+
+  // write range viewer
+  const knob = active ? (blink ? '$26' : '$27') : '$4'
+  const bar = strsplice(
+    '-'.repeat(values.length),
+    value,
+    1,
+    `$green${knob}$${tcolor}`,
+  ).replaceAll('-', '$7')
+  tokenizeAndWriteTextFormat(`${bar} \\`, context)
+  writeCharToEnd(' ', context)
+
+  const up = useCallback<UserInputHandler>(
+    (mods) => {
+      const step = mods.alt ? 10 : 1
+      setValue((state) => Math.min(max, state + step))
+    },
+    [max, value],
+  )
+
+  const down = useCallback<UserInputHandler>(
+    (mods) => {
+      const step = mods.alt ? 10 : 1
+      setValue((state) => Math.max(min, state - step))
+    },
+    [min, value],
+  )
+
+  return <>{active && <UserInput MOVE_LEFT={down} MOVE_RIGHT={up} />}</>
 }
--- zss/gadget/components/panel/textinput.tsx
+++ zss/gadget/components/panel/textinput.tsx
@@ -1,6 +1,13 @@
-import { tokenizeAndWriteTextFormat } from '../../data/textFormat'
+import React, { useState } from 'react'
 
-import { PanelItemProps } from './common'
+import {
+  cacheWriteTextContext,
+  tokenizeAndWriteTextFormat,
+  writeCharToEnd,
+} from '../../data/textFormat'
+import { UserFocus, UserInput } from '../userinput'
+
+import { PanelItemProps, mapTo, strsplice, useBlink } from './common'
 
 export function PanelItemInputText({
   player,
@@ -9,6 +16,91 @@ export function PanelItemInputText({
   args,
   context,
 }: PanelItemProps) {
-  tokenizeAndWriteTextFormat(`$white${label} $greenKrupts`, context)
-  return null
+  const target = mapTo(args[0], '')
+
+  // where does the current value live here ??
+  const blink = useBlink()
+  const [value, setValue] = useState('')
+  const [reset, setReset] = useState('')
+  const [cursor, setCursor] = useState(0)
+  const [focus, setFocus] = useState(false)
+  let tvalue = `${value} `
+  const tlabel = label.trim()
+  const tcolor = active ? 'grey' : 'white'
+
+  // keep stable re-renders
+  cacheWriteTextContext(context)
+
+  if (focus) {
+    if (blink) {
+      tvalue = strsplice(tvalue, cursor, 1, '$219+')
+    }
+    tokenizeAndWriteTextFormat(
+      `$green TX $${tcolor}${tlabel}$green ${tvalue} \\`,
+      context,
+    )
+  } else {
+    tokenizeAndWriteTextFormat(
+      `$green TX $${tcolor}${tlabel}$green ${tvalue} \\`,
+      context,
+    )
+  }
+  writeCharToEnd(' ', context)
+
+  return (
+    <>
+      {active && (
+        <UserInput
+          OK_BUTTON={() => {
+            setReset(value)
+            setFocus(true)
+          }}
+        />
+      )}
+      {focus && (
+        <UserFocus blockhotkeys>
+          <UserInput
+            MOVE_LEFT={() => {
+              setCursor((state) => Math.max(0, state - 1))
+            }}
+            MOVE_RIGHT={() => {
+              setCursor((state) => Math.min(value.length, state + 1))
+            }}
+            CANCEL_BUTTON={() => {
+              // revert changes
+              setValue(reset)
+              setFocus(false)
+            }}
+            OK_BUTTON={() => {
+              // keep changes
+              setFocus(false)
+            }}
+            keydown={(event) => {
+              switch (event.key.toLowerCase()) {
+                case 'delete':
+                  if (value.length > 0) {
+                    setValue((state) => strsplice(state, cursor, 1))
+                  }
+                  break
+                case 'backspace':
+                  if (cursor > 0) {
+                    setValue((state) => strsplice(state, cursor - 1, 1))
+                    setCursor((state) => Math.max(0, state - 1))
+                  }
+                  break
+              }
+
+              if (
+                event.key.length === 1 &&
+                value.length < context.width * 0.5
+              ) {
+                setValue((state) => strsplice(state, cursor, 0, event.key))
+                setCursor((state) => state + 1)
+              }
+            }}
+          />
+        </UserFocus>
+      )}
+    </>
+  )
 }
