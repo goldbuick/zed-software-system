588713afff30473162ce02ad2ed77af102898f01
goldbuick <whitlark@gmail.com>
2024-01-03 20:44:28 -0500

    thoughts

---
+++ zss/system/firmware/assembler.ts
@@ -0,0 +1,18 @@
+import { createFirmware } from '../firmware'
+
+export const STUB_FIRMWARE = createFirmware(
+  (chip, name) => {
+    return [false, undefined]
+  },
+  (chip, name, value) => {
+    return [false, undefined]
+  },
+).command('stub', (chip, words) => {
+  return 0
+})
+
+/*
+What is assembler firmware ?
+it is one of the engine firmwares
+that drives editing codepage content
+*/
