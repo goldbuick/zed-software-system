b624146336e43f414e4365dfc6e6b8963ff35853
goldbuick <whitlark@gmail.com>
2024-02-26 11:23:27 -0500

    centralizing typing utils

--- zss/mapping/types.ts
+++ zss/mapping/types.ts
@@ -1,3 +1,10 @@
+import equal from 'fast-deep-equal'
+import { isDefined } from 'ts-extras'
+
+export function isdefined(word: any) {
+  return isDefined(word)
+}
+
 export function isNumber(word: any): word is number {
   return typeof word === 'number'
 }
@@ -20,4 +27,8 @@ export function isArray(word: any): word is any[] {
 
 export function isMaybeArray(word: any): word is any[] | undefined {
   return Array.isArray(word) || word === undefined
+}
+
+export function isEqual(a: any, b: any) {
+  return equal(a, b)
 }
