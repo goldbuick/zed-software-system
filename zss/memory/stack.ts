c7926df7b5cea94591ad4dca30e57193408d9bb2
goldbuick <whitlark@gmail.com>
2024-03-04 19:46:07 -0500

    .

--- zss/memory/stack.ts
+++ zss/memory/stack.ts
@@ -1,9 +1,10 @@
-/*
+export enum FRAME_TYPE {
+  APP, // handles ticking the needed boards
+  VIEW, // a view into book state
+  EDIT, // shared crdt backed book state + copy buffers
+}
 
-what is stack?
-a stack is a set of views/interfaces into books
-
-*/
-
-export enum STACK_TYPE {}
-//
+export type FRAME =
+  | { type: FRAME_TYPE.APP }
+  | { type: FRAME_TYPE.VIEW }
+  | { type: FRAME_TYPE.EDIT }
