2090e96f7d47d46f5403249dd24e64983f03af8d
goldbuick <whitlark@gmail.com>
2024-04-19 23:56:05 -0400

    tsconfig still needs paths

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -59,6 +59,9 @@ module.exports = {
     '@typescript-eslint/no-unsafe-argument': 'off',
     '@typescript-eslint/no-unsafe-assignment': 'off',
     '@typescript-eslint/no-unsafe-member-access': 'off',
+    "@typescript-eslint/consistent-type-definitions": [
+      'error', 'type'
+    ],
     'no-console': [
       'error',
       {
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import arraybuffer from 'vite-plugin-arraybuffer'
 import path from 'path'
+
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+import arraybuffer from 'vite-plugin-arraybuffer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: './zss/terminal/',
-  plugins: [react(), tsconfigPaths(), arraybuffer()],
+  plugins: [react(), arraybuffer()],
   resolve: {
     alias: {
       zss: path.resolve(__dirname, './zss'),
