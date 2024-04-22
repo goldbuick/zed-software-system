29249a3f39903791ab8e124182f390a15a7ed77f
goldbuick <whitlark@gmail.com>
2024-04-22 09:52:28 -0400

    showing build info in title

--- README.md
+++ README.md
@@ -3,3 +3,8 @@ A zzt-inspired web based software framework
 
 ## brain dump kanban
 https://trello.com/invite/b/fCgw9JWS/ATTI5dd28eb3e32cffabffe26939fbe1ed64B421F81F/zss
+
+## versioning
+
+https://github.com/phips28/gh-action-bump-version/tree/master?tab=readme-ov-file#workflow
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,36 @@
+import { execSync } from 'node:child_process'
 import path from 'path'
 
 import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import arraybuffer from 'vite-plugin-arraybuffer'
 
+import pkg from './package.json'
+
 // https://vitejs.dev/config/
-export default defineConfig({
-  root: 'zss/terminal/',
-  envPrefix: 'ZSS_',
-  plugins: [react(), arraybuffer()],
-  resolve: {
-    alias: {
-      zss: path.resolve(__dirname, './zss'),
+export default defineConfig(({ mode }) => {
+  // Load app-level env vars to node-level env vars.
+  process.env = {
+    ...process.env,
+    ZSS_BRANCH_NAME: execSync('git rev-parse --abbrev-ref HEAD')
+      .toString()
+      .trimEnd(),
+    ZSS_BRANCH_VERSION: pkg.version,
+    ZSS_COMMIT_HASH: execSync('git rev-parse HEAD').toString().trimEnd(),
+    ZSS_COMMIT_MESSAGE: execSync('git show -s --format=%s')
+      .toString()
+      .trimEnd(),
+    ...loadEnv(mode, process.cwd()),
+  }
+
+  return {
+    root: 'zss/terminal/',
+    envPrefix: 'ZSS_',
+    plugins: [react(), arraybuffer()],
+    resolve: {
+      alias: {
+        zss: path.resolve(__dirname, './zss'),
+      },
     },
-  },
+  }
 })
