1a026d9746251518954a70c9c51d0180d6ebab5b
goldbuick <whitlark@gmail.com>
2024-07-15 23:11:27 -0400

    stubbed out remaining codepage firmware

--- zss/firmware/loader.ts
+++ zss/firmware/loader.ts
@@ -4,26 +4,34 @@ import { CODE_PAGE_TYPE } from 'zss/memory/codepage'
 
 import { ALL_FIRMWARE } from './all'
 import { AUDIO_FIRMWARE } from './audio'
+import { BOARD_FIRMWARE } from './board'
+import { CHARSET_FIRMWARE } from './charset'
 import { CLI_FIRMWARE } from './cli'
 import { GADGET_FIRMWARE } from './gadget'
 import { OBJECT_FIRMWARE } from './object'
+import { PALETTE_FIRMWARE } from './palette'
+import { TERRAIN_FIRMWARE } from './terrain'
 
 const firmwares: Record<string, FIRMWARE> = {
   all: ALL_FIRMWARE,
   cli: CLI_FIRMWARE,
+  board: BOARD_FIRMWARE,
   audio: AUDIO_FIRMWARE,
+  terrain: TERRAIN_FIRMWARE,
   gadget: GADGET_FIRMWARE,
   object: OBJECT_FIRMWARE,
+  palette: PALETTE_FIRMWARE,
+  charset: CHARSET_FIRMWARE,
 }
 
 export const CODE_PAGE_FIRMWARE = {
   [CODE_PAGE_TYPE.ERROR]: [],
   [CODE_PAGE_TYPE.CLI]: ['all', 'audio', 'cli'],
-  [CODE_PAGE_TYPE.BOARD]: ['all', 'audio', 'gadget'],
-  [CODE_PAGE_TYPE.OBJECT]: ['all', 'audio', 'object', 'gadget'],
-  [CODE_PAGE_TYPE.TERRAIN]: ['all', 'audio'],
-  [CODE_PAGE_TYPE.CHARSET]: ['all'],
-  [CODE_PAGE_TYPE.PALETTE]: ['all'],
+  [CODE_PAGE_TYPE.BOARD]: ['all', 'audio', 'gadget', 'mods'],
+  [CODE_PAGE_TYPE.OBJECT]: ['all', 'audio', 'object', 'gadget', 'mods'],
+  [CODE_PAGE_TYPE.TERRAIN]: ['all', 'audio', 'terrain', 'gadget', 'mods'],
+  [CODE_PAGE_TYPE.CHARSET]: ['all', 'audio', 'charset', 'gadget', 'mods'],
+  [CODE_PAGE_TYPE.PALETTE]: ['all', 'audio', 'palette', 'gadget', 'mods'],
 }
 
 export type FIRMWARE_NAME = keyof typeof CODE_PAGE_FIRMWARE
---
+++ zss/firmware/terrain.ts
@@ -0,0 +1,21 @@
+import { createfirmware } from 'zss/firmware'
+
+export const TERRAIN_FIRMWARE = createfirmware({
+  get(chip, name) {
+    return [false, undefined]
+  },
+  set(chip, name, value) {
+    return [false, undefined]
+  },
+  shouldtick(chip) {
+    //
+  },
+  tick(chip) {
+    //
+  },
+  tock(chip) {
+    //
+  },
+}).command('stub', (chip, words) => {
+  return 0
+})
