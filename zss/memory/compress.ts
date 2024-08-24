a699573e0a3082d0b03b31e5cb451421e87259e1
goldbuick <whitlark@gmail.com>
2024-08-24 01:47:35 -0400

    .

--- zss/memory/compress.ts
+++ zss/memory/compress.ts
@@ -1,13 +1,43 @@
-import { Parsed, object, dynamicArrayOf, i32, string, bool } from 'typed-binary'
+import * as bin from 'typed-binary'
 import { MAYBE } from 'zss/mapping/types'
 
 import { BOOK, MAYBE_BOOK } from './book'
 
-export function compressbook(book: BOOK): MAYBE<Uint8Array> {
+const binword = bin.generic(
+  {
+    //
+  },
+  {
+    //
+  },
+)
+
+const binbook = bin.object({
+  id: bin.string,
+  name: bin.string,
+  tags: bin.dynamicArrayOf(bin.string),
+  flags: bin.dynamicArrayOf(
+    bin.object({
+      player: bin.string,
+      values: bin.dynamicArrayOf(
+        bin.object({
+          name: bin.string,
+          number: bin.optional(bin.f32),
+          string: bin.optional(bin.string),
+          array: bin.optional(bin.dynamicArrayOf()),
+        }),
+      ),
+    }),
+  ),
+})
+
+type BIN_BOOK = bin.Parsed<typeof binbook>
+
+export async function compressbook(book: BOOK): Promise<MAYBE<Uint8Array>> {
   return undefined
 }
 
 // import json into book
-export function decompressbook(bytes: Uint8Array): MAYBE_BOOK {
+export async function decompressbook(bytes: Uint8Array): Promise<MAYBE_BOOK> {
   return undefined
 }
