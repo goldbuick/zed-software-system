e6aac2f24e751bdab9ca561423ee8b8a98691737
goldbuick <whitlark@gmail.com>
2024-08-30 11:26:43 -0400

    I think once I fix the mods api, I’ll push a new build

--- zss/memory/compress.ts
+++ zss/memory/compress.ts
@@ -1,9 +1,48 @@
-import { fromBase64, toBase64 } from '@smithy/util-base64'
+import { decode, encode } from 'base64-arraybuffer'
 import { gzip, gunzip } from 'fflate'
 import * as bin from 'typed-binary'
 import { ispresent } from 'zss/mapping/types'
 
 import { BIN_BOOK, BOOK, exportbook, importbook } from './book'
+
+// data encoding for urls
+function base64urltobase64(base64UrlString: string) {
+  // Replace non-url compatible chars with base64 standard chars
+  const base64 = base64UrlString.replace(/-/g, '+').replace(/_/g, '/')
+
+  // Pad out with standard base64 required padding characters if missing
+  const missingPadding = '='.repeat((4 - (base64.length % 4)) % 4)
+
+  return base64 + missingPadding
+}
+
+function base64tobase64url(base64String: string) {
+  // Replace base64 standard chars with url compatible chars
+  return base64String.replace(/\+/g, '-').replace(/\//g, '_')
+}
+
+function base64decode(data: string): Uint8Array {
+  return new Uint8Array(decode(atob(data)))
+}
+
+function base64encode(data: Uint8Array): string {
+  return btoa(encode(data))
+}
+
+function base64urltouint8array(base64String: string) {
+  // base64 de-sanitizing
+  const base64 = base64urltobase64(base64String)
+
+  // base64 decoding
+  return base64decode(base64)
+}
+
+function uint8arraytobase64url(bytes: Uint8Array) {
+  const base64 = base64encode(bytes)
+
+  // base64 sanitizing
+  return base64tobase64url(base64)
+}
 
 const BIN_BOOKS = bin.dynamicArrayOf(BIN_BOOK)
 
@@ -28,7 +67,7 @@ export async function compressbooks(books: BOOK[]) {
           reject(err)
         }
         if (asciibytes) {
-          resolve(toBase64(asciibytes))
+          resolve(uint8arraytobase64url(asciibytes))
         }
       },
     )
@@ -38,7 +77,7 @@ export async function compressbooks(books: BOOK[]) {
 // import json into book
 export async function decompressbooks(base64bytes: string) {
   return new Promise<BOOK[]>((resolve, reject) => {
-    const asciibytes = fromBase64(base64bytes)
+    const asciibytes = base64urltouint8array(base64bytes)
     gunzip(asciibytes, {}, (err, binbooks) => {
       if (err) {
         reject(err)
