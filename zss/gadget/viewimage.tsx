ede67c3f8efb31a3f99fadf4aef41529669dbe12
goldbuick <whitlark@gmail.com>
2025-07-28 20:44:26 -0400

    .

--- zss/gadget/viewimage.tsx
+++ zss/gadget/viewimage.tsx
@@ -1,8 +1,9 @@
 import { useLoader } from '@react-three/fiber'
-import { Suspense } from 'react'
+import { Suspense, useEffect } from 'react'
 import { TextureLoader } from 'three'
 import { RUNTIME } from 'zss/config'
 
+import { updateTexture } from './display/textures'
 import { ShadeBoxDither } from './graphics/dither'
 import { useMedia } from './hooks'
 import { UserFocus, UserInput } from './userinput'
@@ -16,9 +17,21 @@ type TapeShowImage = {
 
 function TapeShowImage({ url, drawwidth, drawheight }: TapeShowImage) {
   const texture = useLoader(TextureLoader, url)
+
+  useEffect(() => {
+    texture.flipY = true
+    updateTexture(texture)
+  }, [texture])
+
+  const { width: imagewidth = 1, height: imageheight = 1 } =
+    texture?.image ?? {}
+  const scale = Math.min(drawwidth / imagewidth, drawheight / imageheight)
+  const withdrawwidth = imagewidth * scale
+  const withdrawheight = imageheight * scale
+
   return (
     <mesh>
-      <planeGeometry args={[drawwidth, -drawheight]} />
+      <planeGeometry args={[withdrawwidth, withdrawheight]} />
       <meshBasicMaterial map={texture} />
     </mesh>
   )
@@ -35,8 +48,10 @@ export function TapeViewImage() {
 
   const rightedge = screensize.cols - 1
   const bottomedge = screensize.rows - 1
-  const drawwidth = screensize.cols * RUNTIME.DRAW_CHAR_WIDTH()
-  const drawheight = screensize.rows * RUNTIME.DRAW_CHAR_HEIGHT()
+  const drawwidth = (screensize.cols - 4) * RUNTIME.DRAW_CHAR_WIDTH()
+  const drawheight = (screensize.rows - 2) * RUNTIME.DRAW_CHAR_HEIGHT()
+  const centerwidth = screensize.cols * RUNTIME.DRAW_CHAR_WIDTH() * 0.5
+  const centerheight = screensize.rows * RUNTIME.DRAW_CHAR_HEIGHT() * 0.5
   return (
     viewimage && (
       <UserFocus>
@@ -51,7 +66,11 @@ export function TapeViewImage() {
             right={rightedge}
             bottom={bottomedge}
           />
-          <group position={[drawwidth * 0.5, drawheight * 0.5, 1]}>
+          <group
+            position={[centerwidth, centerheight, 1]}
+            scale-x={-1}
+            rotation-z={Math.PI}
+          >
             <Suspense fallback={null}>
               <TapeShowImage
                 url={viewimage}
