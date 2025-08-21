7304dd1259e899bb338b30afa77e6b7a1ec9828d
goldbuick <whitlark@gmail.com>
2025-08-20 23:26:53 -0400

    .

--- zss/feature/tts.ts
+++ zss/feature/tts.ts
@@ -66,8 +66,9 @@ async function handlenexttts(synth: MAYBE<AUDIO_SYNTH>) {
     Math.round(audiobuffer.duration * 1000) - 1000,
   )
 
-  await waitfor(waittime)
-  nextisactive = false
+  setTimeout(() => {
+    nextisactive = false
+  }, waittime)
 }
 
 export function ttsqueue(
