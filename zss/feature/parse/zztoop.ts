1ab78a56ef3ef97714fd0fb1cfb472ce96d7bfa4
goldbuick <whitlark@gmail.com>
2025-11-13 19:19:36 -0500

    keyword and command mapping

--- zss/feature/parse/zztoop.ts
+++ zss/feature/parse/zztoop.ts
@@ -1,3 +1,49 @@
+function mapkeyword(line: string, keyword: string) {
+  let scrubbed = line
+
+  // label check
+  if (scrubbed.startsWith(`:${keyword}`)) {
+    scrubbed = scrubbed.replace(`:${keyword}`, `:_${keyword}`)
+  }
+
+  // comment check
+  if (scrubbed.trim() === `'${keyword}`) {
+    scrubbed = scrubbed.replace(`'${keyword}`, `'_${keyword}`)
+  }
+
+  // send check
+  if (scrubbed.startsWith(`#${keyword}`)) {
+    scrubbed = scrubbed.replace(`#${keyword}`, `#_${keyword}`)
+  }
+
+  // shortcut send check
+  if (scrubbed.endsWith(` ${keyword}`)) {
+    scrubbed = scrubbed.replace(` ${keyword}`, ` _${keyword}`)
+  }
+
+  // result
+  return scrubbed
+}
+
+function mapcommand(line: string, command: string, mapped: string) {
+  let scrubbed = line
+
+  // command check
+  if (scrubbed.trimStart().startsWith(`#${command}`)) {
+    scrubbed = scrubbed.replace(`#${command}`, `#${mapped}`)
+  }
+
+  // shortcut command check
+  if (
+    scrubbed.trimStart().startsWith('#') &&
+    scrubbed.includes(` ${command}`)
+  ) {
+    scrubbed = scrubbed.replace(` ${command}`, ` ${mapped}`)
+  }
+
+  return scrubbed
+}
+
 export function zztoop(content: string) {
   const lines = content.replaceAll(/\r?\n|\r/g, '\n').split('\n')
   return lines
@@ -24,14 +70,15 @@ export function zztoop(content: string) {
         }
       }
 
-      // TODO: revisit this when we get
-      // a file with a repro
-      if (
-        scrubbed.includes('#do') ||
-        scrubbed.includes(':do') ||
-        scrubbed.endsWith(' do')
-      ) {
-        //
+      // handle zss reserved words being used for labels
+      scrubbed = mapkeyword(scrubbed, 'do')
+      scrubbed = mapkeyword(scrubbed, 'done')
+
+      // handle weave commands that map to zss
+      scrubbed = mapcommand(scrubbed, 'fgplay', 'play')
+
+      if (scrubbed.includes('do') || scrubbed.includes('done')) {
+        console.info('>>>', scrubbed)
       }
 
       return scrubbed
