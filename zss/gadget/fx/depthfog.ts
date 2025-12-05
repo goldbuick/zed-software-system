40d2460d7af76c52f3fc34f41e3f25608adaa335
goldbuick <whitlark@gmail.com>
2025-12-04 21:37:20 -0500

    drop unused

--- zss/gadget/fx/depthfog.ts
+++ zss/gadget/fx/depthfog.ts
@@ -2,17 +2,6 @@ import { EffectProps, wrapEffect } from '@react-three/postprocessing'
 import { BlendFunction, Effect, EffectAttribute } from 'postprocessing'
 
 const depthfogfragshader = `
-// adapted from https://www.shadertoy.com/view/Mlt3z8
-float bayerDither2x2( vec2 v ) {
-  return mod( 3.0 * v.y + 2.0 * v.x, 4.0 );
-}
-
-float bayerDither4x4( vec2 v ) {
-  vec2 P1 = mod( v, 2.0 );
-  vec2 P2 = mod( floor( 0.5  * v ), 2.0 );
-  return 4.0 * bayerDither2x2( P1 ) + bayerDither2x2( P2 );
-}
-
 float exponentialIn(float t) {
   return t == 0.0 ? t : pow(2.0, 10.0 * (t - 1.0));
 }
