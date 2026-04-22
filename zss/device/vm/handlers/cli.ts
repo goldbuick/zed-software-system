bd0662b3230630f9bb21dca26b514ddaa5ed7ef3
goldbuick <whitlark@gmail.com>
2026-04-21 21:57:48 -0400

    .

--- zss/device/vm/handlers/cli.ts
+++ zss/device/vm/handlers/cli.ts
@@ -1,18 +1,11 @@
 import type { DEVICE } from 'zss/device'
 import type { MESSAGE } from 'zss/device/api'
-import { ispresent, isstring } from 'zss/mapping/types'
 import { memoryrepeatclilast, memoryruncli } from 'zss/memory/runtime'
 
 export function handlecli(_vm: DEVICE, message: MESSAGE): void {
-  if (!isstring(message.player) || !isstring(message.data)) {
-    return
-  }
   memoryruncli(message.player, message.data)
 }
 
 export function handleclirepeatlast(_vm: DEVICE, message: MESSAGE): void {
-  if (!ispresent(message.player)) {
-    return
-  }
   memoryrepeatclilast(message.player)
 }
