fff17b11089d58b7efa7045d85b8d3255c8a1870
goldbuick <whitlark@gmail.com>
2026-05-11 12:43:26 -0400

    .

--- zss/device/vm/boardrunnerpushupdates.ts
+++ zss/device/vm/boardrunnerpushupdates.ts
@@ -4,9 +4,9 @@ import { perfmeasure } from 'zss/perf/ui'
 import { boardrunnerboundarymemorysync } from './boardrunnerboundarysync'
 import { boardrunnermemorysync } from './boardrunnermemorysync'
 
-export function boardrunnerpushupdates(vm: DEVICE) {
+export function boardrunnerpushupdates(vm: DEVICE, showlog = false) {
   perfmeasure('vm:boardrunnersync', () => {
-    boardrunnermemorysync(vm, true)
-    boardrunnerboundarymemorysync(vm, true)
+    boardrunnermemorysync(vm, showlog)
+    boardrunnerboundarymemorysync(vm, showlog)
   })
 }
