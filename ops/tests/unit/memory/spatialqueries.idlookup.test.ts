497f76bb1909359b70cc66b3e7c4a7c06ca5a660
goldbuick <whitlark@gmail.com>
2026-08-24 10:20:52 -0400

    .

--- ops/tests/unit/memory/spatialqueries.idlookup.test.ts
+++ ops/tests/unit/memory/spatialqueries.idlookup.test.ts
@@ -1,6 +1,5 @@
 import { memorylistboardelementsbyidnameorpts } from 'zss/memory/spatialqueries'
 import { BOARD, BOARD_ELEMENT } from 'zss/memory/types'
-import { NAME } from 'zss/words/types'
 
 describe('memorylistboardelementsbyidnameorpts', () => {
   const MIXED_ID = 'sid_hd0VuNrSi0Cg'
@@ -25,15 +24,6 @@ describe('memorylistboardelementsbyidnameorpts', () => {
   it('finds object by exact-case id', () => {
     const board = makeboard()
     expect(memorylistboardelementsbyidnameorpts(board, [MIXED_ID])).toEqual([
-      object,
-    ])
-  })
-
-  it('finds object when id is NAME-folded like scroll hyperlink chips', () => {
-    const board = makeboard()
-    const folded = NAME(MIXED_ID)
-    expect(folded).not.toBe(MIXED_ID)
-    expect(memorylistboardelementsbyidnameorpts(board, [folded])).toEqual([
       object,
     ])
   })
